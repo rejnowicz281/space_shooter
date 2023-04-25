3979441e11f0f7a9ce9e2211960bc0ca051fe337
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-04-25 20:51:29 +0200

    Add spaceship to the game

--- main.py
+++ main.py
@@ -4,16 +4,32 @@ import pygame
 pygame.init()
 
 # Create screen
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((700, 700))
 
 # Title and Icon
 pygame.display.set_caption("Space Shooter")
-icon = pygame.image.load('rocket.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 310
+playerY = 500
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 
 # Game Loop
 running = True
 while running:
+
+    screen.fill((82, 100, 150))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
